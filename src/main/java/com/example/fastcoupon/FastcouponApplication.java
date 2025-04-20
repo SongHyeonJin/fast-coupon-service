afdc31e4be9545d6b062666299937f067a070a87
HyeonJin Song <shwj1602@naver.com>
2025-04-20 20:40:55 +0900

    init: 프로젝트 기본 구조 및 설정 추가

---
+++ src/main/java/com/example/fastcoupon/FastcouponApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fastcoupon/FastcouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
