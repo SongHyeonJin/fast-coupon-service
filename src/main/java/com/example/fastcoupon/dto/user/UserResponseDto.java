76c3ae69bb427b688d3828b771070a340959f381
HyeonJin Song <shwj1602@naver.com>
2025-04-21 18:57:41 +0900

    feat(user): 로그인 시 세션에 userId, email, role 저장 로직 추가

--- src/main/java/com/example/fastcoupon/dto/user/UserResponseDto.java
+++ src/main/java/com/example/fastcoupon/dto/user/UserResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.fastcoupon.dto.user;
 
+import com.example.fastcoupon.enums.UserRoleEnum;
 import lombok.Getter;
 
 @Getter
@@ -7,11 +8,15 @@ public class UserResponseDto {
 
     private Long userId;
     private String name;
+    private String email;
+    private UserRoleEnum role;
     private String msg;
 
-    public UserResponseDto(Long userId, String name, String msg) {
+    public UserResponseDto(Long userId, String name, String email, UserRoleEnum role, String msg) {
         this.userId = userId;
         this.name = name;
+        this.email = email;
+        this.role = role;
         this.msg = msg;
     }
 }
