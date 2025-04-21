61b4a26e78364d770d74a74b5f855d53351575a4
HyeonJin Song <shwj1602@naver.com>
2025-04-21 19:08:08 +0900

    feat: 권한 예외 Enum(NOT_ALLOW) 추가

--- src/main/java/com/example/fastcoupon/enums/ExceptionEnum.java
+++ src/main/java/com/example/fastcoupon/enums/ExceptionEnum.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionEnum {
     EMAIL_DUPLICATION(HttpStatus.BAD_REQUEST.value(),"USER", "중복된 이메일입니다."),
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "USER", "사용자를 찾을 수 없습니다."),
-    WRONG_PASSWORD(HttpStatus.BAD_REQUEST.value(), "USER", "비밀번호가 일치하지 않습니다.");
+    WRONG_PASSWORD(HttpStatus.BAD_REQUEST.value(), "USER", "비밀번호가 일치하지 않습니다."),
+    NOT_ALLOW(HttpStatus.FORBIDDEN.value(), "권한이 없습니다.", "USER");
 
     private final int status;
     private final String type;
