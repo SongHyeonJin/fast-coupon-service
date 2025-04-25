c28b9f211f5f625f907c722f627aeb69a5422c88
HyeonJin Song <shwj1602@naver.com>
2025-04-25 17:51:23 +0900

    feat(scheduler): 쿠폰 만료 처리 스케줄러 구현

--- src/main/java/com/example/fastcoupon/repository/CouponRepository.java
+++ src/main/java/com/example/fastcoupon/repository/CouponRepository.java
@@ -3,5 +3,11 @@ package com.example.fastcoupon.repository;
 import com.example.fastcoupon.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+    List<Coupon> findAllByExpiredAtBetween(LocalDateTime from, LocalDateTime now);
+
 }
